c829d9ad771bfd73754dff469c67bdcd76afe2b1
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-07 19:57:18 -0300

    criando tabela de candidato

--- src/main/java/com/brunobandeiraf/vacancy_management/modules/candidate/CandidateEntity.java
+++ src/main/java/com/brunobandeiraf/vacancy_management/modules/candidate/CandidateEntity.java
@@ -3,17 +3,30 @@ package com.brunobandeiraf.vacancy_management.modules.candidate;
 import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
-
+import org.hibernate.annotations.CreationTimestamp;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 // @Data add para todos os atributos
 @Data
+@Entity(name = "candidate")
+// Nome da Tabela
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+  
+  //@Column(name="nome_usuário")
   private String name;
 
   //@Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
@@ -28,6 +41,10 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
   // Lombok 
   // baseado em anotações, não é preciso add os getters e setters
