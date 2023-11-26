1427514b60dc0aec37369521336a04daa4d8e77b
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-25 21:11:37 -0300

    refatorando jbocontroller

---
+++ src/main/java/com/brunobandeiraf/vacancy_management/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,11 @@
+package com.brunobandeiraf.vacancy_management.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+  private String description;
+  private String benefits;
+  private String level;
+}
--- src/main/java/com/brunobandeiraf/vacancy_management/modules/company/entities/JobEntity.java
+++ src/main/java/com/brunobandeiraf/vacancy_management/modules/company/entities/JobEntity.java
@@ -13,10 +13,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "job")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
   @Id
