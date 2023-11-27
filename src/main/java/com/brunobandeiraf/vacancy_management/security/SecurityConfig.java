82188ab92c37b8a6015f06ea112260ce095f11ec
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-26 23:57:27 -0300

    controller authCandidate

--- src/main/java/com/brunobandeiraf/vacancy_management/security/SecurityConfig.java
+++ src/main/java/com/brunobandeiraf/vacancy_management/security/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> {
           auth.requestMatchers("/candidate/").permitAll()
               .requestMatchers("/company/").permitAll()
-              .requestMatchers("/auth/company").permitAll();
+              .requestMatchers("/auth/company").permitAll()
+              .requestMatchers("/candidate/auth").permitAll();
           auth.anyRequest().authenticated();
           // Qualquer outra rota, solicita autenticação.
         }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
