0c5cdc72f53dd50496254892fd0d86fd398282a8
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-30 19:04:30 -0300

    filtro de segurança do candidato

--- src/main/java/com/brunobandeiraf/vacancy_management/security/SecurityConfig.java
+++ src/main/java/com/brunobandeiraf/vacancy_management/security/SecurityConfig.java
@@ -15,6 +15,9 @@ public class SecurityConfig {
   @Autowired
   private SecurityFilter securityFilter;
 
+  @Autowired
+  private SecurityCandidateFilter securityCandidateFilter;
+
   @Bean
   // Método dentro da classe já é gerenciado pelo Spring
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,7 +30,9 @@ public class SecurityConfig {
               .requestMatchers("/candidate/auth").permitAll();
           auth.anyRequest().authenticated();
           // Qualquer outra rota, solicita autenticação.
-        }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+        })
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class);
 
 
     return http.build();
