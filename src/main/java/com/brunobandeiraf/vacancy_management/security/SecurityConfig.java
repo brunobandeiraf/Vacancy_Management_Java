4ec62400e8d406c9c957fa4da2593c1d0b5172de
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-12-07 19:53:53 -0300

    configurando swagger

--- src/main/java/com/brunobandeiraf/vacancy_management/security/SecurityConfig.java
+++ src/main/java/com/brunobandeiraf/vacancy_management/security/SecurityConfig.java
@@ -20,6 +20,12 @@ public class SecurityConfig {
   @Autowired
   private SecurityCandidateFilter securityCandidateFilter;
 
+  private static final String[] SWAGGER_LIST = {
+    "/swagger-ui/**",
+    "/v3/api-docs/**",
+    "/swagger-resource/**",
+  };
+
   @Bean
   // Método dentro da classe já é gerenciado pelo Spring
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,7 +35,8 @@ public class SecurityConfig {
           auth.requestMatchers("/candidate/").permitAll()
               .requestMatchers("/company/").permitAll()
               .requestMatchers("/company/auth").permitAll()
-              .requestMatchers("/candidate/auth").permitAll();
+              .requestMatchers("/candidate/auth").permitAll()
+              .requestMatchers(SWAGGER_LIST).permitAll();
           auth.anyRequest().authenticated();
           // Qualquer outra rota, solicita autenticação.
         })
